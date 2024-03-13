6f9ad2b9d9eed12ddec7a93341872f8105ec9f97
youngotutu <60820143+yuvuki@users.noreply.github.com>
2024-03-14 00:15:25 +0100

    rewrite interface

--- account/main.go
+++ account/main.go
@@ -4,28 +4,38 @@ import (
 	"context"
 
 	"github.com/otyang/waas-go"
+	"github.com/otyang/waas-go/currency"
 	"github.com/uptrace/bun"
 )
 
 type IAccountFeature interface {
-	WalletCreate(ctx context.Context, wallet waas.Wallet) (*waas.Wallet, error)
-	WalletGetByID(ctx context.Context, walletID string) (*waas.Wallet, error)
-	WalletUpdate(ctx context.Context, wallet waas.Wallet) (*waas.Wallet, error)
-	WalletList(ctx context.Context, params waas.ListWalletsFilterParams) ([]waas.Wallet, error)
-	WalletGetByUserIDAndCurrencyCode(ctx context.Context, userID, currencyCode string) (*waas.Wallet, error)
+	// Currency
+	CreateCurrency(ctx context.Context, currency currency.Currency) (*currency.Currency, error)
+	UpdateCurrency(ctx context.Context, currency currency.Currency) (*currency.Currency, error)
+	ListCurrencies(ctx context.Context) ([]currency.Currency, error)
+
+	// wallets
+	CreateWallet(ctx context.Context, wallet *waas.Wallet) (*waas.Wallet, error)
+	GetWalletByID(ctx context.Context, walletID string) (*waas.Wallet, error)
+	GetWalletByUserIDAndCurrencyCode(ctx context.Context, userID, currencyCode string) (*waas.Wallet, error)
+	UpdateWallet(ctx context.Context, wallet *waas.Wallet) (*waas.Wallet, error)
+	ListWallet(ctx context.Context, params waas.ListWalletsFilterParams) ([]waas.Wallet, error)
 
 	// Transaction Management
-	TransactionCreate(ctx context.Context, transaction waas.Transaction) (*waas.Transaction, error)
-	TransactionGetByID(ctx context.Context, transactionID string) (*waas.Transaction, error)
-	TransactionUpdate(ctx context.Context, transaction waas.Transaction) (*waas.Transaction, error)
-	TransactionList(ctx context.Context, params waas.ListTransactionsFilterParams) ([]waas.Transaction, error)
+	CreateTransaction(ctx context.Context, transaction *waas.Transaction) (*waas.Transaction, error)
+	GetTransaction(ctx context.Context, transactionID string) (*waas.Transaction, error)
+	UpdateTransaction(ctx context.Context, transaction *waas.Transaction) (*waas.Transaction, error)
+	ListTransaction(ctx context.Context, limit int, params waas.ListTransactionsFilterParams) ([]waas.Transaction, error)
 
+	// actions
 	Credit(ctx context.Context, params waas.CreditWalletParams) (*waas.CreditWalletResponse, error)
 	Debit(ctx context.Context, params waas.DebitWalletParams) (*waas.DebitWalletResponse, error)
 	Swap(ctx context.Context, params waas.SwapRequestParams) (*waas.SwapWalletResponse, error)
 	Transfer(ctx context.Context, params waas.TransferRequestParams) (*waas.TransferResponse, error)
 	Reverse(ctx context.Context, transactionID string) (*waas.ReverseResponse, error)
 }
+
+var _ IAccountFeature = (*Account)(nil)
 
 type Account struct {
 	db bun.IDB
